699708590aa80038a203e5d832ec695b60c33d33
Thomas Blaymire <thomas.blaymire@hermes-europe.co.uk>
2017-12-19 08:46:43 +0000

    Added bcrypt to encrypt user password informaiton

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,8 +1,13 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
 exports.signup = function(req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
+
+    // Enforces that the user must have a username and password 
+    if (!email || !password) {
+        return res.status(442).send({ error: 'You must provide an email and password' });
+    }
 
     // See if a user with the given email exists
     User.findOne({ email: email }, function(err, existingUser) {
@@ -10,15 +15,24 @@ exports.signup = function(req, res, next) {
             return next(err);
         }
 
-    // If a user with email does exist, return an Error
+        // If a user with email does exist, return an Error
         if (existingUser) {
-            return res.status(422).send({ error: 'Email is in use' });
+            return res.status(422).send({ error: "Email is in use" });
         }
 
+        // If a user with email does NOT exist, create and save user record
+        const user = new User({
+            email: email,
+            password: password
+        });
+
+        user.save(function(err) {
+            if (err) {
+                return next(err);
+            }
+
+            // Respond to request indivating the user was created
+            res.json({ success: true });
+        });
     });
-
-
-    // If a user with email does NOT exist, create and save user record
-
-    // Respond to request indivating the user was created
 }
